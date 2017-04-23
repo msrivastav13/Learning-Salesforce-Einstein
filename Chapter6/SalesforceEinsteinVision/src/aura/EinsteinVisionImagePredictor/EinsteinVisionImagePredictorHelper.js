772ef0c259d52e61b79dcbabaeb46c6cca9ee743
Mohith Shrivastava <msrivastav13@gmail.com>
2017-04-23 18:44:58 -0400

    made some enhancements to design of smart predictor

--- Chapter6/SalesforceEinsteinVision/src/aura/EinsteinVisionImagePredictor/EinsteinVisionImagePredictorHelper.js
+++ Chapter6/SalesforceEinsteinVision/src/aura/EinsteinVisionImagePredictor/EinsteinVisionImagePredictorHelper.js
@@ -1,11 +1,13 @@
 ({
     callImagePredictor : function(component, event, helper) {
         var files = event.getSource().get("v.files");
+        var preview = document.querySelector('img');
         var file = files[0];
         var fr = new FileReader();
         var self = this;
            fr.onload = function() {
             var fileContents = fr.result;
+            preview.src = fr.result;
             var base64Mark = 'base64,';
             var dataStart = fileContents.indexOf(base64Mark) + base64Mark.length;
             fileContents = fileContents.substring(dataStart);
@@ -21,12 +23,14 @@
             fileName: file.name,
             base64Data: encodeURIComponent(fileContents), 
             contentType: file.type,
-            modelId : 'PFCOJICP2VD4LYV5M3O4A2XVLU'
+            modelId : component.get("v.modelId")
         });
  
         action.setCallback(this, function(response) {
            var result = response.getReturnValue();
             console.log(result);
+            console.log(result.probabilities[0].label);
+           component.set("v.imageName",result.probabilities[0].label);
         });
          $A.enqueueAction(action); 
     }
